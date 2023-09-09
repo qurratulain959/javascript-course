6e82599efab97b13165382b7746c25d1c64685c6
qurratulain959 <123088553+qurratulain959@users.noreply.github.com>
2023-09-09 19:18:14 +0000

    conversion of data type

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Karachi"
+
+accountEmail = "qurrat@gmail.com"
+accountPassword = "1212122"
+accountCity ="Lahore"
+
+console.log(accountId);
+
+console.table ( [ accountEmail, accountPassword, accountCity])
---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,9 @@
+let score = "33"
+
+// node 01_basic/03_conversionOperation.js
+console.log (typeof score);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log ( booleanIsLoggedIn);
