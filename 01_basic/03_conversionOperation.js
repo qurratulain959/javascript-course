35ff589139262a1b62c4fa416b8686bd90e4d884
qurratulain959 <123088553+qurratulain959@users.noreply.github.com>
2023-09-12 19:02:32 +0000

    dataTypes

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,9 +1,21 @@
 let score = "33"
 
 // node 01_basic/03_conversionOperation.js
-console.log (typeof score);
+//console.log (typeof score);
 
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean (isLoggedIn)
-console.log ( booleanIsLoggedIn);
+//console.log ( booleanIsLoggedIn);
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+
+let str1 = "Hello"
+let str2 = " Qurrat"
+let str3 = str1 + str2
+console.log(str3);
+
---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,24 @@
+//console.log( 2 > 1);
+
+//console.log( null > 0);
+//console.log( null == 0);
+//console.log( null >= 0);
+
+console.log( undefined = 0);
+
+// non primitive (refrence) types:
+// array, object, function
+
+const heros = [ "ironMan", "batMan"];
+// it is an ex of array
+
+let myObj= {
+    name : "Qurrat",
+    age : 24,
+}
+// it is an ex of object
+
+const myFunction = function (){
+    console.log ( "Hello World");
+}
+//it is an ex of function
